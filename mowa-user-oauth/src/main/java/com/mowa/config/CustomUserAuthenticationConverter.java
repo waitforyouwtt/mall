6126cbef8dbca8dc0b3301d61b0080936d44f20c
luoxian <wtt19940512577>
2021-01-25 09:46:46 +0800

    oauth 2.0x

--- mowa-user-oauth/src/main/java/com/mowa/config/CustomUserAuthenticationConverter.java
+++ mowa-user-oauth/src/main/java/com/mowa/config/CustomUserAuthenticationConverter.java
@@ -1,11 +1,15 @@
 package com.mowa.config;
 
+import com.mowa.util.UserJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Component
@@ -16,7 +20,25 @@ public class CustomUserAuthenticationConverter extends DefaultUserAuthentication
 
     @Override
     public Map<String, ?> convertUserAuthentication(Authentication authentication) {
-        
-    }
+        LinkedHashMap response = new LinkedHashMap();
+        String name = authentication.getName();
+        response.put("username",name);
 
+        Object principal = authentication.getPrincipal();
+        UserJwt userJwt = null;
+        if (principal instanceof  UserJwt){
+            userJwt = (UserJwt) principal;
+        }else{
+            //refresh token 默认不去调用userDetailService 获取用户信息，这里我们手动去调用，得到UserJwt
+            UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+            userJwt = (UserJwt) userDetails;
+        }
+        response.put("name",userJwt.getName());
+        response.put("id",userJwt.getId());
+        response.put("company","mowa");
+        if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()){
+            response.put("authorities", AuthorityUtils.authorityListToSet(authentication.getAuthorities()));
+        }
+        return response;
+    }
 }
